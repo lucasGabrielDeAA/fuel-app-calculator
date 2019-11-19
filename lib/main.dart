6a201c72a0af79a0afba645da43da506a7249fe2
lucasGabrielDeAA <lg.technus@gmail.com>
2019-11-19 16:55:35 -0300

    Layout adjustments

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
 
-import 'widgets/inputrow.widget.dart';
+import 'widgets/input.widget.dart';
+import 'widgets/loading-button.widget.dart';
 import 'widgets/logo.widget.dart';
 
 void main() => runApp(MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Combustível',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.deepPurple,
       ),
       home: HomePage(),
     );
@@ -31,8 +32,65 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           Logo(),
-          InputRow(label: "Álcool", textInputController: gasTextController),
-          InputRow(label: "Gasolina", textInputController: alcTextController),
+          Container(
+            child: Column(
+              children: <Widget>[
+                // Container(
+                //   alignment: Alignment.center,
+                //   child: Text(
+                //     "Teste",
+                //     style: TextStyle(
+                //       color: Theme.of(context).primaryColor,
+                //       fontFamily: "Big Shoulders Display",
+                //       fontSize: 40,
+                //       fontWeight: FontWeight.bold,
+                //     ),
+                //   ),
+                //   decoration: BoxDecoration(
+                //     borderRadius: BorderRadius.circular(25),
+                //     color: Colors.white,
+                //   ),
+                //   height: 90,
+                // ),
+                Container(
+                  child: Text(
+                    "Gasolina",
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontFamily: "Big Shoulders Display",
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  margin: EdgeInsets.only(bottom: 5, top: 5),
+                ),
+                Input(textInputController: gasTextController),
+                Container(
+                  child: Text(
+                    "Álcool",
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontFamily: "Big Shoulders Display",
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  margin: EdgeInsets.only(bottom: 5, top: 5),
+                ),
+                Input(textInputController: alcTextController),
+                SizedBox(height: 20),
+                Container(
+                  child: LoadingButton(loading: true, callback: (){}, label: "CALCULAR"),
+                ),
+              ],
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25),
+              color: Colors.white.withOpacity(0.8),
+            ),
+            margin: EdgeInsets.only(left: 30, right: 30, top: 30),
+            padding: EdgeInsets.only(bottom: 20, left: 30, right: 30, top: 10),
+          ),
         ],
       ),
     );
