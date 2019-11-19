5fcc50a346793689fc7d770ad863cf8b43bfba24
lucasGabrielDeAA <lg.technus@gmail.com>
2019-11-19 12:24:23 -0300

    App's main file added

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+
+import 'widgets/inputrow.widget.dart';
+import 'widgets/logo.widget.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Combustível',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: ListView(
+        children: <Widget>[
+          Logo(),
+          InputRow(label: "Álcool", textInputController: new MoneyMaskedTextController()),
+          InputRow(label: "Gasolina", textInputController: new MoneyMaskedTextController()),
+        ],
+      ),
+    );
+  }
+}
